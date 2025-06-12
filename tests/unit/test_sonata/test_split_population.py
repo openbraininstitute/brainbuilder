667b7f59edf3da90d897b86e70d7292a4e54b147
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-06-12 15:07:48 +0200

    Fixed neuron set "someB" in existing tests

--- tests/unit/test_sonata/test_split_population.py
+++ tests/unit/test_sonata/test_split_population.py
@@ -533,6 +533,7 @@ def _check_biophysical_nodes(path, has_virtual, has_external, from_subcircuit=Fa
             "mtype_a": {"mtype": "a"},
             "someA": {"node_id": [0, 1], "population": "A"},
             "allB": {"node_id": [0, 1, 2, 3], "population": "B"},
+            "someB": {"node_id": [1, 2], "population": "B"},
             "noC": {"node_id": [], "population": "C"},
         }
 
